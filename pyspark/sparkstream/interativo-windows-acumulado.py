37718307ab7ec7690162426619033c56ba05e63a
Marcio Junior Vieira <marcio@ambientelivre.com.br>
2025-02-21 08:30:46 -0300

    Create interativo-windows-acumulado.py

---
+++ pyspark/sparkstream/interativo-windows-acumulado.py
@@ -0,0 +1,33 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode, split, window, current_timestamp
+
+# Criar sessão do Spark
+spark = SparkSession.builder \
+    .appName("SocketStreamSlidingWindow") \
+    .getOrCreate()
+
+# Criar DataFrame Streaming a partir de um socket TCP
+socketDF = spark.readStream \
+    .format("socket") \
+    .option("host", "localhost") \
+    .option("port", 9999) \
+    .load()
+
+# Adicionar uma coluna de timestamp (tempo de chegada do dado)
+socketDF = socketDF.withColumn("timestamp", current_timestamp())
+
+# Transformação: dividir as linhas em palavras
+words = socketDF.select(explode(split(socketDF.value, " ")).alias("word"), "timestamp")
+
+## Para Manter Tudo 
+wordCounts = words.groupBy("word").count()
+
+query = wordCounts.writeStream \
+    .outputMode("update") \  # Modo update mantém o estado
+    .format("console") \
+    .option("truncate", "false") \
+    .start()
+
+query.awaitTermination()
+
+
